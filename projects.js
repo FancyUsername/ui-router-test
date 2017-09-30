5948f6b9ce6429c29657f5881aae025f0d9f3cd1
John Ny <anewone@gmx.de>
2017-09-30 02:25:25 +0200

    better project filter search

--- projects.js
+++ projects.js
@@ -5,13 +5,22 @@
         },
 
         templateUrl: "projects.html",
-        
-        controller: function() {
+
+        controller: function () {
+            var vm = this;
             this.filterChanged = filterChanged;
-            
+
+            this.search = function (item) {
+                if (!vm.projectFilter) {
+                    return true;
+                }
+                
+                return _.find(_.toArray(_.pick(item, ['id', 'name', 'projectlead'])), p => p.toLowerCase().indexOf(vm.projectFilter.toLowerCase()) >= 0);
+            };
+
             function filterChanged(searchText) {
                 this.projectFilter.id = searchText;
             }
         }
-    })
+    });
 })();
